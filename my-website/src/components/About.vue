7d2fbd750e35536eb34a70220963bc39d9b32cc3
tcollard <tcollard@student.42.fr>
2019-12-29 17:11:09 +0100

    feat: 🎸 (component) Add experience template

--- my-website/src/components/About.vue
+++ my-website/src/components/About.vue
@@ -4,90 +4,92 @@
             <h1>ABOUT</h1>
             <p>Thibault Collard étudiant @ 42 depuis 2017</p>
         </span>
-        <span class="link">
+        <div class="link">
             <a class="contentText" href="https://fr.linkedin.com/in/thibault-collard-b8228168" target="_blank">
-                <img id="imgLinkedIn" src="../assets/linkedIn.svg" alt="linkedIn">
+                <img class="img" src="../assets/linkedIn.svg" alt="linkedIn">
             </a>
             <a class="contentText" href="https://github.com/tcollard" target="_blank">
-                <img id="imgGithub" src="../assets/github.png" alt="github">
+                <img class="img" src="../assets/github.svg" alt="github">
             </a>
-        </span>
+        </div>
 
         <br>
         <h2 class="title">Expérience Professionnelle</h2>
         <hr>
-        <span class="experienceBlock">
-            <p class="subtitle">2019 - Développeur Full Stack</p>
-            <p class="duration">6 mois - Internship</p>
-            <span>
-                Au sein de la startup
-                <a class="contentText" href="https://neocity.fr/" target="_blank">Neocity</a>,
-                spécialisée dans les applications mobiles pour les villes.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim perferendis optio molestias reprehenderit error inventore recusandae voluptas! Atque nemo suscipit reprehenderit quasi. Aperiam ratione a culpa amet rem suscipit ipsa!
-            </span>
-        </span>
+        <ul>
+            <Experience v-for="xp in experiences" :key="xp" :xp="xp"></Experience>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
+    import Experience from './Experience.vue';
+
     export default ({
         name: 'About',
+        components: {
+            Experience,
+        },
+        data() {
+            return {
+                experiences: [
+                    {
+                        date: '2019',
+                        poste: 'Développeur Full Stack',
+                        during: '6 mois',
+                        type: 'Intership',
+                        content: `Au sein de la startup
+                            <a class="contentText" href="https://neocity.fr/" target="_blank">Neocity</a>
+                            spécialisée dans les applications mobiles pour les villes.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim perferendis optio molestias reprehenderit error inventore recusandae voluptas! Atque nemo suscipit reprehenderit quasi. Aperiam ratione a culpa amet rem suscipit ipsa!`,
+                    },
+                    {
+                        date: '2020',
+                        poste: ' TEST Développeur Full Stack',
+                        during: '16 mois',
+                        type: 'CDI',
+                        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+                    },  
+                ],
+            };
+        },
     })
 </script>
 
 <style scoped>
-.contentText {
-    text-decoration: none;
-    color: black;
-}
 
-.introText {
-    text-decoration: none;
-    color: black;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
+    .introText {
+        text-decoration: none;
+        color: black;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
-.link {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
-}
+    .link {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
 
-.title {
-    text-align: center;
-    font-size: 1.5em;
-    margin: 0;
-}
+    .title {
+        text-align: center;
+        font-size: 1.5em;
+        margin: 0;
+    }
 
-.subtitle {
-    font-size: 1.17em;
-    font-weight: bold;
-    margin: 5px;
-}
+    .img {
+        width: 42px;
+        margin: 10px;
+    }
+    /* #imgLinkedIn {
+        width: 7%;
+    }
 
-.duration {
-    font-size: 1.1em;
-    font-weight: bold;
-    margin: 5px;
-}
-
-.experienceBlock {
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    margin: 15px;
-}
-
-#imgLinkedIn {
-    width: 7%;
-}
-
-#imgGithub {
-    width: 40%;
-}
+    #imgGithub {
+        width: 40%;
+    } */
 
 </style>
---
+++ my-website/src/components/Experience.vue
@@ -0,0 +1,45 @@
+<template>
+    <span class="experienceBlock">
+        <p class="subtitle">{{ xp.date }} - {{ xp.poste }}</p>
+        <p class="duration">{{ xp.during }} - {{ xp.type }}</p>
+        <span v-html="xp.content"></span>
+    </span>
+</template>
+
+<script lang="ts">
+    export default ({
+        name: 'Experience',
+        props: {
+            xp:{
+                type: Array,
+                required: true,
+            },
+        },
+    })
+</script>
+
+<style>
+    .subtitle {
+        font-size: 1.17em;
+        font-weight: bold;
+        margin: 5px;
+    }
+
+    .duration {
+        font-size: 1.1em;
+        font-weight: bold;
+        margin: 5px;
+    }
+
+    .experienceBlock {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin: 15px;
+    }
+    
+    .contentText {
+        text-decoration: none;
+        color: black;
+    }
+</style>
