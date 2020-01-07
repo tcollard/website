be2fe10ea936cfe47d64a612c57c70dd620b95e4
tcollard <tcollard@student.42.fr>
2020-01-07 12:42:19 +0100

    feat: 🎸 (skills) Init list skills

---
+++ my-website/src/components/Skills.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="skills">
+        <h1>Skills</h1>
+        <div v-for="skill in skills" :key="skill" class="listSkills">
+            <ProgressBar :skill="skill"></ProgressBar>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProgressBar from './ProgressBar.vue'
+
+export default {
+    name: 'Skills',
+    components: {
+        ProgressBar,
+    },
+    data() {
+        return {
+            skills: [
+                {
+                    name: 'C',
+                    rate: '90',
+                },
+                                {
+                    name: 'UNIX / SHELL',
+                    rate: '90',
+                },
+                {
+                    name: 'DART /FLUTTER',
+                    rate: '60',
+                },
+                {
+                    name: 'ANGULAR',
+                    rate: '70',
+                },
+                {
+                    name: 'VueJs',
+                    rate: '70',
+                },
+                {
+                    name: 'HTML / CSS',
+                    rate: '80',
+                },
+                {
+                    name: 'JS / NodeJS',
+                    rate: '85',
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.skills {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    margin-left: 10%;
+    margin-right: 10%;
+}
+
+.listSkills {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    text-align: center;
+}
+</style>
