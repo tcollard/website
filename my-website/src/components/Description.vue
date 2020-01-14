1298258077d7b71d3ff152943ce1c45f9ea3aeb9
tcollard <tcollard@student.42.fr>
2020-01-14 21:27:20 +0100

    feat: 🎸 (Descriptiom / Portfolio) Change content text

--- my-website/src/components/Description.vue
+++ my-website/src/components/Description.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="description">
         <div class="title">
-            <span>THIBAULT COLLARD</span>
-            <span>Dev full stack JS</span>
+            <span class="name">THIBAULT COLLARD</span>
+            <span class="post">Dev full stack JS</span>
             <h1>À propos</h1>
         </div>
         <div class="descriptionContent text">
             <span class="text">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio quis deleniti recusandae neque impedit facilis? Ut placeat possimus incidunt. Ipsa, voluptate? Distinctio culpa assumenda iusto nisi ea aut tempora autem molestiae enim natus vero quas consequatur officiis, dicta voluptatem? Officia laboriosam quo a nesciunt qui modi alias tempora nemo ad recusandae, fugit hic? At mollitia voluptatibus, explicabo fuga eaque accusamus veritatis quia nihil atque quasi unde magni cupiditate! Sequi velit asperiores impedit iure ipsum iste est deserunt cumque quas corrupti beatae illo dolor perspiciatis quasi, atque veniam vero quae eius vel tempore quidem a similique ratione! Magnam quisquam aliquid inventore.
+                {{description}}
             </span>
-            <span>Link download CV</span>
+            <!-- <span>Link download CV</span> -->
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Description'
+    name: 'Description',
+    data () {
+        return {
+            description: 'Aprés avoir été formé à 42 Paris, j\'ai pu travailler dans le développement web et d\'application cross-platform.\n Je m\'interesse à différentes technologies telles que Flutter / vueJS.'
+        }
+    }
 }
 </script>
 
@@ -43,5 +48,18 @@ export default {
   border-radius: 10px;
   background-color: rgb(51, 59, 80);
   text-align: center;
+}
+
+.name {
+    font-family: 'Roboto Slab';
+    font-size: 5em;
+    font-weight: bold;
+}
+
+.post {
+    font-family: 'Roboto Slab';
+    font-size: 3em;
+    font-weight: bold;
+    color: orange;
 }
 </style>
--- my-website/src/components/PortfolioResume.vue
+++ my-website/src/components/PortfolioResume.vue
@@ -3,9 +3,9 @@
         <h1>Portfolio</h1>
         <div class="container">
             <div class="project" v-for="(project, index) in projects" :key="index">
-                <div class="content">{{project.title}}</div>
-                <span class="text">{{project.description}}</span>
-                <a :href="project.link" target="_blank">Lien</a>
+                <div class="title">{{project.title}}</div>
+                <span class="htmlText" v-html="project.description"></span>
+                <a class="link text" :href="project.link" target="_blank">Lien</a>
             </div>
         </div>
     </div>
@@ -20,25 +20,25 @@ export default {
                 {
                     title: '42SH',
                     img: require('../assets/term.gif'),
-                    description: 'Réalisation d\'un interpréteur de commande UNIX tel bash',
+                    description: '<p class="text">Réalisation d\'un interpréteur de commande UNIX tel bash</p>',
                     link: 'https://github.com/tcollard/42sh',
                 },
                 {
                     title: 'IOTA',
                     img: require('../assets/term.gif'),
-                    description: 'Poc d\'une smart grid utilisant le tangle IOTA, et réalisant des micro-transactions entre différents acteurs',
+                    description: '<p class="text">PoC d\'une smart grid utilisant le tangle IOTA, et réalisant des micro-transactions entre différents acteurs</p>',
                     link: 'https://github.com/tcollard/Smart_Grid_IOTA',
                 },
                 {
                     title: 'GAME JAM',
                     img: require('../assets/term.gif'),
-                    description: 'Réalisation d\'un jeux lors de la gloabal game jam, avec Francçois Corbel (Dev / Game Designer), Lucien Coycault (Graphic Designer), Jeff Guerin (Sound Designer), et moi même',
+                    description: '<p class="text">Réalisation d\'un jeux lors de la gloabal game jam, avec <a class="link" href="https://francoiscorbel.com/" target="_blank">Francçois Corbel (Dev / Game Designer)</a>, <a class="link" href="http://www.luciencoycault.fr/" target="_blank">Lucien Coycault (Graphic Designer)</a>, <a class="link" href="https://soundcloud.com/fed-j" target="_blank">Jeff Guerin (Sound Designer)</a>, et moi même</p>',
                     link: 'https://francoiscorbel.com/night-crawlerz/',
                 },
                 {
                     title: 'FLUTTER APP',
                     img: require('../assets/term.gif'),
-                    description: 'Application permettant de jouer des sons, ainsi que d\'en enregistrer',
+                    description: '<p class="text">Application permettant de jouer des sons, ainsi que d\'en enregistrer</p>',
                     link: 'https://github.com/tcollard/mySoundApp',
                 },
             ]
@@ -63,16 +63,43 @@ export default {
     justify-content: space-evenly;
     width: 100%;
     height: 100%;
+    align-items: center;
 }
 
 .project {
-    width: 100%;
+    width: 50%;
     height: 100%;
     display: flex;
     flex-direction: column;
     margin-bottom: 2%;
-    border: 2px solid white;
-    border-radius: 50px;
+    border-radius: 10px;
+    background-color: rgb(51, 59, 80);
+}
+
+.title {
+    color: orange;
+}
+
+.img {
+    width: 100%;
+    height: 100%;
+}
+
+.link {
+    text-decoration: none;
+    color: white;
+}
+
+.htmlText >>> .link {
+    text-decoration: none;
+    color: white;
+}
+
+.htmlText >>> .text {
+    font-family: 'Roboto';
+    font-size: 1em;
+    line-height: 24px;
+    font-weight: 300;
 }
 
 </style>
