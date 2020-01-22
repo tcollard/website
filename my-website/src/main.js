44aef76856c56dc1f644b8443d0530839b59eda0
tcollard <tcollard@student.42.fr>
2020-01-22 18:15:51 +0100

    feat: 🎸 (i18n) Imprort & config i18n

--- my-website/src/main.js
+++ my-website/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vueScrollto from 'vue-scrollto'
-
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 Vue.use(vueScrollto)
+Vue.use(i18n)
 
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
